72b6e1320569f1c911371ec18eebf1d1409c66cc
John G <john.gugliotti@gmail.com>
2021-10-09 17:48:08 -0400

    implement CSRS methods, add class name methods to retirement systems and set retsys in emp object init

--- retirement_eligibility_functions.py
+++ retirement_eligibility_functions.py
@@ -44,6 +44,7 @@ class EmployeeAttributes:
         self.dob = self.to_date(dob)
 
         self.eod_date = self.to_date(eod_date)
+        self.retirement_system = FERS if self.eod_date >= datetime(1987, 1, 1) else CSRS
         self.appointment_type = appointment_type
 
         self.years_of_service = math.floor(((datetime.now() - self.eod_date).days / 365.25))
@@ -75,6 +76,13 @@ class EmployeeAttributes:
 
 ## FERS
 class FERS:
+    """
+        https://www.opm.gov/retirement-services/fers-information/eligibility
+    """
+    @classmethod
+    def __str__(cls) -> str:
+        return 'FERS'
+
     @staticmethod
     def min_retirement_age(emp: EmployeeAttributes = None) -> dict:
         if emp.birth_year <= 1947:
@@ -135,9 +143,8 @@ class FERS:
             print("fall out")
             return False
 
-
     @staticmethod
-    def deferred_retirement_eligibility(enp: EmployeeAttributes) -> bool:
+    def deferred_retirement_eligibility(emp: EmployeeAttributes) -> bool:
         """
         Deferred Retirement
 
@@ -152,36 +159,29 @@ class FERS:
             MRA	30
             MRA	10
         """
-
+        return (emp.age >= 62 and emp.years_of_service >= 5) \
+               or (emp.meets_minimum_age_criteria and emp.years_of_service >= 30) \
+               or (emp.meets_minimum_age_criteria and emp.years_of_service >= 10)
         return emp.years_of_service >= 5
 
     @staticmethod
     def disability_retirement_eligibility(emp: EmployeeAttributes) -> bool:
+        """
+            Age / Years of Service
+            Any Age	/ 18 months
+            Special Requirements : You must have become disabled, while employed in a position subject to FERS,
+            because of a disease or injury, for useful and efficient service in your current position.
+            The disability must be expected to last at least one year. Your agency must certify that it is unable
+            to accommodate your disabling medical condition in your present position and
+            that it has considered you for any vacant position in the same agency at the same grade/pay level
+            , within the same commuting area, for which you are qualified for reassignment.
+        :param emp:
+        :return: boolean True if employee meets qualifications and false otherwise
+        """
         return (datetime.now() - emp.eod_date).days >= (1.5 * 365.25)  # 18 months
 
-    ## CSRS
-    """
-    
-    There are five categories of benefits under the Civil Service Retirement System (CSRS). 
-    Eligibility is based on your age and the number of years of creditable service and any other special requirements. 
-    In addition, you must have served in a position subject to CSRS coverage for one of the last two years 
-        before your retirement. 
-    If you meet one of the following sets of requirements, you may be eligible for an immediate retirement benefit. 
-    An immediate annuity is one that begins within 30 days after your separation.
-    
-    Optional
-    If you leave Federal service before you meet the age and service requirements for an immediate retirement benefit, 
-        you may be eligible for deferred retirement benefits. 
-    To be eligible, you must have at least 5 years of creditable civilian service and be age 62.
-    """
 
-    """
-    Age	Years of Service
-    62	5
-    60	20
-    55	30
-    """
-
+    @staticmethod
     def immediate_retirement_benefit(emp: EmployeeAttributes) -> float:
         """
             If you retire at the MRA with at least 10, but less than 30 years of service,
@@ -216,6 +216,33 @@ class FERS:
 
 
 class CSRS:
+    """
+    https://www.opm.gov/retirement-services/csrs-information/
+
+    There are five categories of benefits under the Civil Service Retirement System (CSRS).
+    Eligibility is based on your age and the number of years of creditable service and any other special requirements.
+    In addition, you must have served in a position subject to CSRS coverage for one of the last two years
+        before your retirement.
+    If you meet one of the following sets of requirements, you may be eligible for an immediate retirement benefit.
+    An immediate annuity is one that begins within 30 days after your separation.
+
+    Optional
+    If you leave Federal service before you meet the age and service requirements for an immediate retirement benefit,
+        you may be eligible for deferred retirement benefits.
+    To be eligible, you must have at least 5 years of creditable civilian service and be age 62.
+    """
+
+    """
+    Age	Years of Service
+    62	5
+    60	20
+    55	30
+    """
+
+    @classmethod
+    def __str__(cls) -> str:
+        return 'CSRS'
+
     @staticmethod
     def retirement_eligibility(emp: EmployeeAttributes):
         return (emp.years_of_service >= 5 and emp.age >= 62) \
@@ -234,34 +261,48 @@ class CSRS:
     Any Age	25
     """
 
-    def early_retirement_eligibility(emp: EmployeeAttributes):
-        return (emp.years_of_service >= 20 and emp.age >= 50) or (emp.years_of_service >= 25)
+    def early_retirement_eligibility(emp: EmployeeAttributes) -> bool:
+        """Special Provision Retirement
+            Special Requirements: You must retire under special provisions for:
+                air traffic controllers
+                law enforcement and firefighter personnel
+                nuclear materials courier
+                supreme Court Police
+                Capitol Police
 
-    """Special Provision Retirement
-    Special Requirements: You must retire under special provisions 
-        for air traffic controllers or law enforcement and firefighter personnel, nuclear materials courier
-        , Supreme Court Police and Capitol Police.
-    
-    Age	Years of Service
-    50	20
-    Any Age *	25
-    * ONLY air traffic controllers can retire at any age with 25 years of service as an air traffic controller.
-    """
+            Age	Years of Service
+            50	20
+            Any Age *	25
+            * ONLY air traffic controllers can retire at any age with 25 years of service as an air traffic controller.
+        """
+        return (emp.years_of_service >= 20 and emp.age >= 50) or \
+               (emp.years_of_service >= 25 and emp.age >= (16 + 25))
 
-    def special_provision(emp: EmployeeAttributes):
-        return
+    @staticmethod
+    def discontinued_service_retirement(emp: EmployeeAttributes) -> bool:
+        """
+        Discontinued Service
+        Age	Years of Service
+        50	20
+        Any Age	25
+        Special Requirements: Your separation is involuntary and not a removal for misconduct or delinquency.
+        """
+        return (emp.years_of_service >= 20 and emp.age >= 50) or \
+               (emp.years_of_service >= 25 and emp.age >= (16 + 25))
 
-    """
-    Discontinued Service
-    Age	Years of Service
-    50	20
-    Any Age	25
-    Special Requirements: Your separation is involuntary and not a removal for misconduct or delinquency.
-    """
+    @staticmethod
+    def special_provision(emp: EmployeeAttributes) -> bool:
+        return (emp.years_of_service >= 20 and emp.age >= 50) \
+               or (emp.years_of_service >= 25 and emp.age >= (16 + 25))
 
-    """
-    Disability
-    Age	Years of Service
-    Any Age	5
-    Special Requirements: You must be disabled for useful and efficient service in your current position and any other vacant position at the same grade or pay level within your commuting area and current agency for which you are qualified. You must have been disabled prior to retirement and the disability should be expected to last for more than one year.
-    """
+    def disability_retirement(emp: EmployeeAttributes):
+        """
+        Disability
+        Age	Years of Service
+        Any Age	5
+        Special Requirements: You must be disabled for useful and efficient service in your current position and any other
+            vacant position at the same grade or pay level within your commuting area and current agency for which you
+            are qualified. You must have been disabled prior to retirement and the disability should be expected to
+            last for more than one year.
+        """
+        return emp.years_of_service >= 5 and emp.age >= (16 + 5)  ## min employment age plus five years
